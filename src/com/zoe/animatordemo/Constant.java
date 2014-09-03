d9b8000e689964105127be868b8676316a85f505
仔仔 <370985116@qq.com>
2014-09-03 17:39:52 +0800

    first commit

---
+++ src/com/zoe/animatordemo/Constant.java
@@ -0,0 +1,7 @@
+package com.zoe.animatordemo;
+
+import com.zoe.animatordemo.SwitchAnimationUtil.AnimationType;
+
+public class Constant {
+    public static AnimationType mType = AnimationType.ALPHA;
+}
